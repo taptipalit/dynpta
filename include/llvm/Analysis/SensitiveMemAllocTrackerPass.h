82727bb693e781c0c654595759a4a23135d99be9
Tapti Palit <tpalit@cs.stonybrook.edu>
2020-02-11 12:19:48 -0500

    Sensitive mem alloc tracker pass - Phase 2
    Former-commit-id: 5c2f04a87fe73153d743165ef762833499fe00ef

---
+++ include/llvm/Analysis/SensitiveMemAllocTrackerPass.h
@@ -0,0 +1,71 @@
+#ifndef SMAT_H_
+#define SMAT_H_
+
+#include <utility>      // std::pair, std::make_pair
+#include <llvm/Pass.h>
+#include <llvm/IR/Instructions.h>
+#include "llvm/Support/CommandLine.h"
+#include <llvm/IR/Module.h>
+#include <llvm/IR/InstIterator.h>
+#include <llvm/Support/raw_ostream.h>
+#include <vector>
+#include <set>
+#include <map>
+
+
+class SensitiveMemAllocTrackerPass: public llvm::ModulePass {
+
+public:
+    /// Pass ID
+    static char ID;
+
+    /// Constructor needs TargetLibraryInfo to be passed to the AliasAnalysis
+    SensitiveMemAllocTrackerPass() : llvm::ModulePass(ID) {
+
+    }
+
+    /// Destructor
+    ~SensitiveMemAllocTrackerPass() { }
+
+    /// LLVM analysis usage
+    virtual inline void getAnalysisUsage(llvm::AnalysisUsage &au) const {
+        // declare your dependencies here.
+        /// do not intend to change the IR in this pass,
+        au.setPreservesAll();
+    }
+
+    virtual bool runOnModule(llvm::Module& module);
+
+    std::vector<llvm::CallInst*>& getSensitiveMemAllocCalls() {
+        return sensitiveMemAllocCalls;
+    }
+
+
+private:
+
+    std::set<llvm::Function*> mallocRoutines;
+
+    std::vector<llvm::GetElementPtrInst*> sensitiveGepPtrs;
+
+    std::vector<llvm::StoreInst*> storesAtSensitivePtrs;
+
+    std::vector<llvm::CallInst*> sensitiveMemAllocCalls;
+
+    void collectLocalSensitiveAnnotations(llvm::Module&);
+
+    void findAllSensitiveGepPtrs(llvm::Value*);
+
+    void findStoresAtSensitivePtrs();
+
+    void findMemAllocsReachingSensitivePtrs();
+
+};
+
+namespace llvm {
+    class ModulePass;
+    class Module;
+
+    ModulePass *createSensitiveMemAllocTrackerPass();
+}
+
+#endif
