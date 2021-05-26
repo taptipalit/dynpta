e942307e115b70a15f659195bfd6353a99f1ae1a
Tapti Palit <tpalit@cs.stonybrook.edu>
2021-05-25 22:28:10 -0400

    test example

--- dynpta_tests/test.c
+++ dynpta_tests/test.c
@@ -4,9 +4,11 @@
 
 #define mark_sensitive annotate
 
+void annotate(void*);
+
 int main(void) {
     int *arr = malloc(100);
-    annotate(arr);
+    mark_sensitive(arr);
     for (int i = 0; i < 20; i++) {
         arr[i] = i;
     }
